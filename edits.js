e3db12f197cde108e0811e2bb5c5ab4e0ccb9718
shreeyashrestha <61800644+shreeyashrestha@users.noreply.github.com>
2021-01-12 22:21:58 -0500

    functions

---
+++ edits.js
@@ -0,0 +1,10 @@
+function hamburg()
+{
+	var x = document.getElementById("nav_cen");
+	if (x.className === "navbar") {
+		x.className += " responsive";
+	} else
+	{
+		x.className = "navbar";
+	}
+}
