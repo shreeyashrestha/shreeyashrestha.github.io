8b42b741f3e7cf8c1c68363ac856d7fe2979667a
shreeyashrestha <61800644+shreeyashrestha@users.noreply.github.com>
2021-01-18 16:56:19 -0500

    edit

--- edits.js
+++ edits.js
@@ -9,28 +9,4 @@ function hamburg()
 	}
 
 }
-if (window.matchMedia("(min-width: 768px)").matches) {
-    let mousePosX = 0,
-        mousePosY = 0,
-        mouseCircle = document.getElementById("moucursor");
-
-    document.onmousemove = (e) => {
-        mousePosX = e.pageX;
-        mousePosY = e.pageY;
-    };
-
-    let delay = 6,
-        revisedMousePosX = 0,
-        revisedMousePosY = 0;
-
-    function delayMouseFollow() {
-        requestAnimationFrame(delayMouseFollow);
-
-        revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
-        revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
-
-        mouseCircle.style.top = revisedMousePosY + "px";
-        mouseCircle.style.left = revisedMousePosX + "px";
-    }
-    delayMouseFollow();
-}
+ 
