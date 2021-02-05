d44d3defd89d4ba47639edf891e976beace758dd
shreeyashrestha <61800644+shreeyashrestha@users.noreply.github.com>
2021-02-05 11:54:21 -0500

    updated

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-    $(".button a").click(function(){
-        $(".overlay").fadeToggle(200);
-       $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
-});
-$('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);   
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
-});
-
 $(document).ready(function(){ 
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
